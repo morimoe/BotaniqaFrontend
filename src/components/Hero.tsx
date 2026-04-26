02d632bd5c730d5e1430d8caf605a3cf1a2d89ad
anastasia <anastasia.tischevici@isa.utm.md>
2026-04-26 11:55:46 +0300

    add Hero and Footer

---
+++ src/components/Hero.tsx
@@ -0,0 +1,11 @@
+function Hero() {
+  return (
+    <section>
+      <h2>Добро пожаловать в Botaniqa </h2>
+      <p>Растения для вашего дома и офиса</p>
+      <button>Перейти к каталогу</button>
+    </section>
+  )
+}
+
+export default Hero
